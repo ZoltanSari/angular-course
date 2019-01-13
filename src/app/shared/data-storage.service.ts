e07f612090b59ea1fd295bc3c845115fa914a4d9
ZoltanSari <Kingshapy@gmail.com>
2019-01-13 17:48:27 +0100

    progress the store recipe method

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpRequest } from '@angular/common/http';
 import { RecipeService } from '../services/recipe.service';
 import { Recipe } from '../components/recipes/recipe.model';
 
@@ -7,15 +7,25 @@ import { Recipe } from '../components/recipes/recipe.model';
   providedIn: 'root'
 })
 export class DataStorageService {
-  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+  private dataStorageUrl = 'https://ng-recipe-book-e0b40.firebaseio.com/recipes.json';
+
+  constructor(private http: HttpClient,
+              private recipeService: RecipeService) {}
 
   storeRecipes() {
-    return this.http.put('https://ng-recipe-book-e0b40.firebaseio.com/recipes.json',
-      this.recipeService.getRecipes(), {observe: 'body'});
+    // return this.http.put('https://ng-recipe-book-e0b40.firebaseio.com/recipes.json',
+    //   this.recipeService.getRecipes(),
+    //   {
+    //     observe: 'body',
+    //     // params: new HttpParams('auth', token)
+    //   });
+    const request = new HttpRequest('PUT', this.dataStorageUrl,
+      this.recipeService.getRecipes(), {reportProgress: true});
+    return this.http.request(request);
   }
 
   getRecipes() {
-    this.http.get<Recipe[]>('https://ng-recipe-book-e0b40.firebaseio.com/recipes.json')
+    this.http.get<Recipe[]>(this.dataStorageUrl)
       .subscribe(
         (recipes) => {
           this.recipeService.setRecipe(recipes);
