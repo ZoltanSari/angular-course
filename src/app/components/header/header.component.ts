9e11a3fc8a00e7f74da6f25d79909ef849043df6
ZoltanSari <Kingshapy@gmail.com>
2019-01-13 17:23:21 +0100

    import data storage service and call the method to get recipes from DB

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,25 @@
-import {Component, OnInit} from '@angular/core';
+import { Component } from '@angular/core';
+import { DataStorageService } from '../../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-  ngOnInit(): void {
+export class HeaderComponent {
+
+  constructor(private dataStorageService: DataStorageService) {}
+
+  onSaveData() {
+    this.dataStorageService.storeRecipes()
+      .subscribe(
+        (response) => {
+          console.log(response);
+    }
+      );
+  }
+
+  onFetchData() {
+    this.dataStorageService.getRecipes();
   }
 }
