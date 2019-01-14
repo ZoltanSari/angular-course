af437e0b21143100a7018a55db7cd055ba988c64
ZoltanSari <Kingshapy@gmail.com>
2019-01-14 14:15:30 +0100

    create logging interceptor

--- src/app/shared/logging.interceptor.ts
+++ src/app/shared/logging.interceptor.ts
@@ -3,7 +3,7 @@ import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 
-export class LoggingInterceptor implements HttpInterceptor{
+export class LoggingInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
