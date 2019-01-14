c9740d762c45e5f8dd6f573b605af721542ade17
ZoltanSari <Kingshapy@gmail.com>
2019-01-14 14:16:36 +0100

    add sign in and sign up route to the routes and handle the new and edit recipe when the user isn't login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,26 @@
-import {RouterModule, Routes} from '@angular/router';
-import {NgModule} from '@angular/core';
-import {RecipesComponent} from './components/recipes/recipes.component';
-import {ShoppingListComponent} from './components/shopping-list/shopping-list.component';
-import {RecipeStartComponent} from './components/recipes/recipe-start/recipe-start.component';
-import {RecipeDetailComponent} from './components/recipes/recipe-detail/recipe-detail.component';
-import {RecipeEditComponent} from './components/recipes/recipe-edit/recipe-edit.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { RecipesComponent } from './components/recipes/recipes.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
+import { RecipeStartComponent } from './components/recipes/recipe-start/recipe-start.component';
+import { RecipeDetailComponent } from './components/recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './components/recipes/recipe-edit/recipe-edit.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
+import { SigninComponent } from './components/auth/signin/signin.component';
+import { AuthGuardService } from './services/auth-guard.service';
 
 const appRoutes: Routes = [
-  {path: '', redirectTo: '/recipes', pathMatch: 'full'},
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   { path: 'recipes', component: RecipesComponent, children: [
-      {path: '', component: RecipeStartComponent},
-      {path: 'new', component: RecipeEditComponent},
-      {path: ':id', component: RecipeDetailComponent},
-      {path: ':id/edit', component: RecipeEditComponent},
-    ] },
-  {path: 'shopping-list', component: ShoppingListComponent, children: [
+      { path: '', component: RecipeStartComponent },
+      { path: 'new', component: RecipeEditComponent, canActivate: [AuthGuardService]},
+      { path: ':id', component: RecipeDetailComponent },
+      { path: ':id/edit', component: RecipeEditComponent, canActivate: [AuthGuardService]},
     ]},
+  { path: 'shopping-list', component: ShoppingListComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'signin', component: SigninComponent },
 ];
 
 @NgModule({
