a80436378d8aa3c294a0c1f8346eeb10a48053c4
ZoltanSari <Kingshapy@gmail.com>
2019-01-14 14:17:39 +0100

    import firebase and initialize the apikey and the domain

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   loadedFeature = 'recipe';
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
+  }
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCCgJjh3r8AfgPJ6_Nq0SccwrezP8rEzgc',
+      authDomain: 'ng-recipe-book-e0b40.firebaseapp.com',
+    });
   }
 }
